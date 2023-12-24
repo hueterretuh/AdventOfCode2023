f0e48bc35a07e1ae858632bc90cf20550bba5477
Jamin Hüter <jamin@live.at>
2023-12-24 11:13:41 +0100

    -

---
+++ AOC10/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] strings = File.ReadAllLines(@"C:\Users\jamin\OneDrive\Dokumente\AdventOfCode\9.txt");
+        Tile[,] tileMap = new Tile[strings[0].Length,strings.Length];
+
+        for (int y = 0; y < strings.Length; y++)
+        {
+            for(int x = 0; x < strings[y].Length; x++)
+            {
+                char c = strings[y][x];
+                if(c != '.')
+                {
+                    tileMap[x, y] = new Tile(x, y,c);
+
+                }
+            }
+            
+        }
+    }
+
+
+}
+
+public class Tile
+{
+    public Tile(int x, int y, char symb)
+    {
+        this.x = x;
+        this.y = y;
+        Connections = new List<Tile>();
+        this.symb = symb;
+    }
+
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public char symb { get; set; }
+    public List<Tile> Connections { get; set; }
+
+    public bool HasTop { get { return symb == '|' || symb == 'L' || symb == 'J' || symb == 'S'; } }
+
+
+}
