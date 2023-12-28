2ee51dd17533e33177fc2ca54fe9c6c74598be75
Jamin Hüter <jamin@live.at>
2023-12-28 23:49:57 +0100

    15.1

--- AOC15/Program.cs
+++ AOC15/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = File.ReadAllText(@"C:\Users\jamin\OneDrive\Dokumente\AdventOfCode\15.txt");
+
+
+        string[] vals = input.Split(',');
+
+        long res = 0;
+        foreach (var item in vals)
+        {
+            res += hash(item);
+        }
+        Console.WriteLine(res);
+    }
+
+    private static int hash(string s)
+    {
+        int res = 0;
+
+        foreach(char c in s)
+        {
+            res += c;
+            res *= 17;
+              res  %= 256;
+        }
+        return res;
+    }
+}
