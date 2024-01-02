8b8d761a96b78aea985793f64c82f3978d623b53
Jamin Hüter <jamin@live.at>
2024-01-02 18:32:59 +0100

    19.2

--- AOC19/Program.cs
+++ AOC19/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 internal class Program
@@ -19,28 +21,291 @@ internal class Program
         }while (i < strings.Length);
         
         foreach (Rule rule in rules) { rule.SetConditions(rules); }
+        foreach (Rule rule in rules) { rule.SetParent(rules); }
         Rule start = rules.First(o => o.Name == "in");
         int counter = 0;
-        Stopwatch sw = Stopwatch.StartNew();
-        do
+        //   var Ends = rules.Select(o => o.conditions.Select(o => o.AlwaysRule is null ? (o.IfAccepted is null ? null : o.IfAccepted) : o.AlwaysRule).Where(o => o != null).Select(o => o.Name));
+        Console.WriteLine(rules.Count(o => o.Parent == null));
+        Console.WriteLine(rules.Count(o => o.Childs.Count == 0));
+        Console.WriteLine(rules.Sum(o => o.conditions.Count(o => o.CanReject)));
+
+        List<Condition> CanReject = rules.SelectMany(o => o.conditions.Where(o => o.CanReject)).ToList();
+        
+        List<List<Check>> checkList = new List<List<Check>>();
+        foreach (Condition condition in CanReject)
         {
-            string mString = strings[i++];
-            if (start.IsAccepted(mString).Value)
+            List<Check> checks = new List<Check>();
+            Condition cond = condition;
+            Rule Parent = cond.Parent;
+            do
             {
-                int nums = Regex.Matches(mString,@"\d+").Sum(o => int.Parse(o.Value));
-                counter += nums;
+                if (cond.CheckString != null) checks.Add(cond.Check);
+
+                if (Parent.Parent == null) break;
+              
+                
+                cond = Parent.Parent.conditions.First(o => o.Child == Parent);
+               var notCons =  Parent.Parent.conditions.TakeWhile(o => o != cond);
+                if(notCons.Count() > 0)
+                {
+                    checks.AddRange(notCons.Select(o => { var mcheck = o.Check; mcheck.Inverse(); return mcheck; }));
+                }
+                Parent = cond.Parent;
+            } while (true);
+         //   Console.WriteLine(String.Join(" && ", checks.Select(o => o.VariabelName.ToString() + o.Opperator + o.Value)));
+
+            var GroupedChecks = checks.GroupBy(o => o.VariabelName);
+            List<Check> newChecks = new List<Check>();
+            foreach (var checkGroup in GroupedChecks)
+            {
+                if (checkGroup.Count() < 2)
+                {
+                    newChecks.AddRange(checkGroup);
+                    continue;
+                }
+
+                var OpGroups = checkGroup.GroupBy(o => o.Operator);
+                foreach (var opGroup in OpGroups)
+                {
+                    if (opGroup.Count() > 1)
+                    {
+                        if (opGroup.Key == '>')
+                        {
+                            newChecks.Add(new Check(checkGroup.Key, '>', opGroup.Max(o => o.Value)));
+                        }
+                        else
+                        {
+                            newChecks.Add(new Check(checkGroup.Key, '<', opGroup.Min(o => o.Value)));
+                        }
+                    }
+                    else
+                    {
+                        newChecks.AddRange(opGroup);
+                        continue;
+                    }
+                }
             }
-        }while (i < strings.Length);
-        Console.WriteLine(sw.ElapsedMilliseconds);
-        Console.WriteLine(counter.ToString());
+   //         Console.WriteLine(String.Join(" && ", newChecks.Select(o => o.VariabelName.ToString() + o.Opperator + o.Value)));
+            List<Check> newerChecks = new List<Check>();
+
+            var VarGroups = newChecks.GroupBy(o => o.VariabelName);
+
+            foreach (var checkGroup in VarGroups)
+            {
+                if (checkGroup.Count() < 2)
+                {
+                    newerChecks.AddRange(checkGroup);
+                }
+                else
+                {
+                    if (checkGroup.Count() > 2) throw new Exception();
+                    int smallerAsVal = checkGroup.First(o => o.Operator == '<').Value;
+                    int greaterAsVal = checkGroup.First(o => o.Operator == '>').Value;
+                    if (smallerAsVal < greaterAsVal)
+                    {
+                        throw new Exception();
+                    }
+                    newerChecks.Add(new Check(checkGroup.Key, checkGroup.Min(o => o.Value), checkGroup.Max(o => o.Value)));
+                }
+
+            }
+
+            Console.WriteLine(String.Join(" && ", newerChecks.Select(o => o.VariabelName.ToString() + (o.Value2.HasValue ? o.Value : "" )+ o.Operator + (o.Value2.HasValue ? o.Value2: o.Value))));
+            checkList.Add(newerChecks);
+
+        }
+
+      
+        long res = (long)Math.Pow(3999, 4);
+        foreach(var checks in checkList)
+        {
+            long tempRes = (long)Math.Pow(3999, 4-checks.Count);
+
+            foreach (var check in checks)
+            {
+                if(check.Operator == '>')
+                {
+                    tempRes *= 4000 - check.Value-1;
+                }else if(check.Operator == '<') {
+                    tempRes *= check.Value-1;
+                }
+                else
+                {
+                    tempRes *= check.Value2.Value - check.Value -1;
+                }
+            }
+            res -= tempRes;
+        }
+
+        Console.WriteLine(res);
+
+        Int128 res2 = GetCombs(start, new Intervals());
+        Console.WriteLine(res2);
+
+       
+        /*
+    do
+    {
+        string mString = strings[i++];
+        if (start.IsAccepted(mString).Value)
+        {
+            int nums = Regex.Matches(mString,@"\d+").Sum(o => int.Parse(o.Value));
+            counter += nums;
+        }
+    }while (i < strings.Length);
+
+    Console.WriteLine(counter.ToString());
+*/
+    }
+
+    static Int128 GetCombs(Rule start,Intervals intervals)
+    {
+        Int128 res = 0;
+        Intervals localInvervals = (Intervals) intervals.Clone();
+        foreach(var cond in start.conditions)
+        {
+            if (cond.AlwaysReject)
+            {
+                //res += localInvervals.Count;
+                return res;
+            }
+            else if (cond.CanReject)
+            {
+                localInvervals.RemoveNumbers(cond.Check);
+             //   res += GetCombs(cond.Child, intervals);
+            }else if(cond.AlwaysAccept)
+            {
+                res += localInvervals.Count;
+                return res;
+            }else if (cond.CanAccepts)
+            {
+                res += localInvervals.RemoveNumbers(cond.Check);
+
+            }
+            else if (cond.AlwaysRule != null)
+            {
+                res += GetCombs(cond.AlwaysRule, localInvervals);
+            }else
+            {
+                Intervals childIntervals = (Intervals)localInvervals.Clone();
+                childIntervals.RemoveNumbers(cond.Check,true);
+                res +=  GetCombs(cond.Child, childIntervals);
+                localInvervals.RemoveNumbers(cond.Check);
+            }
+        }
+        return res;
     }
 }
+public class Intervals : ICloneable
+{
+    public List<int> Xs { get; set; }
+     public List<int> Ms { get; set; }
+     public List<int> As { get; set; }
+     public List<int> Ss { get; set; }
+    
+    public Int128 Count {get {  return (Int128)Xs.Count * (Int128)Ms.Count * (Int128)As.Count * (Int128)Ss.Count; } }
+    public Intervals()
+    {
+        Xs = Enumerable.Range(1,4000).ToList();
+        Ms = Enumerable.Range(1,4000).ToList() ;
+        As = Enumerable.Range(1, 4000).ToList();
+        Ss = Enumerable.Range(1, 4000).ToList();
+    }
+    public Int128 RemoveNumbers(Check check,bool not = false)
+    {
+        
+        Int128 beginCount = Count;
+        List<int> toBeRemovedFrom = new List<int>();
+        if (check.VariabelName == 'x') toBeRemovedFrom = Xs;
+        else if (check.VariabelName == 'm') toBeRemovedFrom = Ms;
+        else if (check.VariabelName == 'a') toBeRemovedFrom = As;
+        else toBeRemovedFrom = Ss;
+        List<int> nums;
+        if (check.Operator == '<')
+        {
+            if(!not) nums = toBeRemovedFrom.Where(o => o < check.Value).ToList();
+            else nums = toBeRemovedFrom.Where(o => o > check.Value-1).ToList();
 
+        }
+        else
+        {
+            if (!not) nums = toBeRemovedFrom.Where(o => o > check.Value).ToList();
+            else nums = toBeRemovedFrom.Where(o => o < check.Value+1).ToList();
+        }
+        if (nums.Count() > 0)
+        {
+            foreach (int i in nums)
+            {
+                if (toBeRemovedFrom.Contains(i)) toBeRemovedFrom.Remove(i);
+            }
+        }
+        return beginCount - Count;
+    }
+    public object Clone()
+    {
+       return new Intervals { As = new List<int>(this.As), Ms = new List<int>(this.Ms),Ss = new List<int>(this.Ss), Xs = new List<int>(Xs)  };
+    }
+}
+public class Check
+{
+   // public bool Not {  get; set; } = false;
+    public char VariabelName { get; set; }
+    public char Operator { get; set; }
+    public int Value { get; set; }
+    public int? Value2 { get; set; }
+
+    public Check(char VariabelName, char Opperator, int Value)
+    {
+        this.VariabelName = VariabelName;
+        this.Operator = Opperator;
+        this.Value = Value;
+    }
+
+    public Check(char VariabelName, int Value,int Value2)
+    {
+        this.VariabelName = VariabelName;
+        this.Operator = '-';
+        this.Value = Value ;
+        this.Value2 = Value2 ;
+    }
+
+    public void Inverse()
+    {
+        if(Operator == '<')
+        {
+            Value--;
+            Operator = '>';
+        }else if(Operator == '>')
+        {
+            Value++;
+            Operator = '<';
+        }else if(Operator == '-')
+        {
+            Operator = '|';
+        }
+        else
+        {
+            Operator = '-';
+        }
+    }
+}
 public class Rule
 {
   public  string Name { get; set; }
     private string inputString;
   public Condition[] conditions { get; set; }
+
+    public Rule Parent { get; set; }
+
+    public List<Rule> Childs { get
+        {
+            List<Rule> list = new List<Rule>();
+           foreach(var cond in conditions)
+            {
+                list.Add(cond.Child);
+            }
+           return list;
+        } }
 
     public Rule(string input)
     {
@@ -48,6 +313,16 @@ public class Rule
         Name = inputString.Split('{')[0];
     }
 
+    public void SetParent(List<Rule> rules)
+    {
+        if (Name == "in") return;
+
+        var Parents =rules.Where(o => o.conditions.Any(o => o.Child?.Name == Name));
+        if(Parents.Count() > 1)  throw new Exception(); 
+        if(Parents.Count()  == 0) throw new Exception();
+        
+        Parent = Parents.First();
+    }
     public void SetConditions(List<Rule> rules)
     {
         string input = inputString.Split('{')[1];
@@ -58,7 +333,7 @@ public class Rule
         for (int i = 0; i < conditionStrings.Length; i++)
         {
             string condition = conditionStrings[i];
-            conditions[i] = new Condition(condition, rules);
+            conditions[i] = new Condition(condition, rules,this);
         }
     }
 
@@ -76,26 +351,46 @@ public class Rule
 
 public class Condition
 {
-    public bool? AlwaysReject { get; set; } = null;
+    public bool AlwaysReject { get { return AlwaysRejectNullable.HasValue &&  AlwaysRejectNullable.Value; } }
+    public bool AlwaysAccept { get { return AlwaysRejectNullable.HasValue && !AlwaysRejectNullable.Value; } }
+    public bool CanReject { get { return AlwaysReject || RejectedIFTrue.HasValue && RejectedIFTrue.Value; } }
+    public bool CanAccepts { get { return AlwaysAccept || RejectedIFTrue.HasValue && !RejectedIFTrue.Value; } }
+    public bool? AlwaysRejectNullable { get; set; } = null;
     public Rule AlwaysRule { get; set; } = null;
-    char VariabelName { get; set; }
-    string Check { get; set; }
-    bool? RejectedIFTrue { get; set; } = null;
-    Rule IfAccepted { get; set; }
+    public char VariabelName { get; set; }
+    public string CheckString { get; set; }
+    public bool? RejectedIFTrue { get; set; } = null;
+    public Rule IfAccepted { get; set; }
+
+    public Check Check { get
+        {
+            return new Check(VariabelName, CheckString[0], int.Parse(CheckString.Substring(1)));
+        } }
+    public Rule Parent { get; set; }
+    public Rule Child { get
+        {
+            List<Rule> result = new List<Rule>();
+            if(AlwaysRule != null) result.Add(AlwaysRule);
+            if(IfAccepted != null) result.Add(IfAccepted);
+            if (result.Count > 1) throw new Exception();
+            return result.Count == 0 ? null : result.First();
+        } }
+    
 
 
-    public Condition(string input,List<Rule> Rules)
+    public Condition(string input,List<Rule> Rules,Rule Parent)
     {
+        this.Parent = Parent;
         if (!input.Contains(':'))
         {
-            if (input == "A") AlwaysReject = false;
-            else if (input == "R") AlwaysReject = true;
+            if (input == "A") AlwaysRejectNullable = false;
+            else if (input == "R") AlwaysRejectNullable = true;
             else AlwaysRule = Rules.First(o => o.Name == input);
         }
         else
         {
             VariabelName = input.First();
-            Check = input.Substring(1).Split(':')[0];
+            CheckString = input.Substring(1).Split(':')[0];
 
             string IfYes = input.Split(':')[1];
 
@@ -108,9 +403,9 @@ public class Condition
 
     public bool? Procces(string input)
     {
-        if (AlwaysReject.HasValue && AlwaysReject.Value) return false;
-        else if(AlwaysReject.HasValue && !AlwaysReject.Value) return true;
-        else if(!AlwaysReject.HasValue && AlwaysRule != null) return AlwaysRule.IsAccepted(input);
+        if (AlwaysRejectNullable.HasValue && AlwaysRejectNullable.Value) return false;
+        else if(AlwaysRejectNullable.HasValue && !AlwaysRejectNullable.Value) return true;
+        else if(!AlwaysRejectNullable.HasValue && AlwaysRule != null) return AlwaysRule.IsAccepted(input);
         else
         {
             string constring = input.Substring(1,input.Length -2);
@@ -118,16 +413,16 @@ public class Condition
             string con = cons.First(o => o[0] == VariabelName);
 
             bool isTrue = false;
-            if (Check[0] == '<')
+            if (CheckString[0] == '<')
             {
-                isTrue = int.Parse(con.Substring(2)) < int.Parse(Check.Substring(1));
-            }else if (Check[0] == '>')
+                isTrue = int.Parse(con.Substring(2)) < int.Parse(CheckString.Substring(1));
+            }else if (CheckString[0] == '>')
             {
-                isTrue = int.Parse(con.Substring(2))  > int.Parse(Check.Substring(1));
+                isTrue = int.Parse(con.Substring(2))  > int.Parse(CheckString.Substring(1));
             }
             else
             {
-                isTrue = int.Parse(con.Substring(2)) == int.Parse(Check.Substring(1));
+                isTrue = int.Parse(con.Substring(2)) == int.Parse(CheckString.Substring(1));
             }
             if(!isTrue) return null;
             if(RejectedIFTrue.HasValue && RejectedIFTrue.Value) return false;
